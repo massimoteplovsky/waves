e453a15ebf373132492756588c0b7f298ff6e2a8
maXimalist <www.maximalist@mail.ru>
2019-08-04 17:39:35 +0300

    changes

--- server/middleware/admin.js
+++ server/middleware/admin.js
@@ -1,6 +1,6 @@
 let admin = (req, res, next) => {
   if(req.user.role === 0){
-    return res.send("You are not allowed to add new brands!!!");
+    return res.send("You are not allowed to do this action");
   }
 
   next();
