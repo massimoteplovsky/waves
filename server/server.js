b5141c2881ab999dd8a453f361cf9fa7f47d4dae
maXimalist <www.maximalist@mail.ru>
2019-08-04 20:59:25 +0300

    d

--- server/server.js
+++ server/server.js
@@ -865,7 +865,7 @@ app.post("/api/users/make_user_history", auth, (req, res) => {
 if(process.env.NODE_ENV === "production"){
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
   })
 }
 
